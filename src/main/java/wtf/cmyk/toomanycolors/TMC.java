654e3ef351a0274a1272e761d01ac4db5070eee5
Josh Walker <walker75@buffalo.edu>
2020-06-23 22:10:36 -0400

    Initial Commit

---
+++ src/main/java/wtf/cmyk/toomanycolors/TMC.java
@@ -0,0 +1,16 @@
+package wtf.cmyk.toomanycolors;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Successfully enabled TooManyColors!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Successfully enabled TooManyColors!");
+    }
+}
