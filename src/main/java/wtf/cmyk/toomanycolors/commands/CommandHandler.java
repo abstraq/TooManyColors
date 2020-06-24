b3f6af09dc5c928b2a9e17c1eda92c270489dad8
abstraq <abstraq@outlook.com>
2020-06-24 18:21:17 -0400

    feat: add tab completions

--- src/main/java/wtf/cmyk/toomanycolors/commands/CommandHandler.java
+++ src/main/java/wtf/cmyk/toomanycolors/commands/CommandHandler.java
@@ -1,18 +1,23 @@
 package wtf.cmyk.toomanycolors.commands;
 
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
+import wtf.cmyk.toomanycolors.TMC;
 import wtf.cmyk.toomanycolors.utils.MessageUtils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 interface CommandInterface {
     boolean onCommand(Player player, Command cmd, String commandLabel, String[] args);
 }
 
-public class CommandHandler implements CommandExecutor {
+public class CommandHandler implements TabExecutor {
     private static final HashMap<String, CommandInterface> subCommands = new HashMap<>();
 
     public void register(String name, CommandInterface command) {
@@ -46,5 +51,30 @@ public class CommandHandler implements CommandExecutor {
             return true;
         }
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        ArrayList<String> suggestions = new ArrayList<>();
+        if(sender instanceof Player) {
+            if (args.length == 1) {
+                List<String> sub = Arrays.asList("help", "set", "del", "list");
+                if(args[0].equals("")) {
+                    suggestions.addAll(sub);
+                } else {
+                    suggestions.addAll(sub.stream().filter(s -> s.startsWith(args[0].toLowerCase())).collect(Collectors.toList()));
+                }
+                return suggestions;
+            }
+
+            if (args.length > 0 && exists(args[0])) {
+                HashMap<String, String> placeholderMap = TMC.getInstance().getProvider().getAllPlaceholders(((Player) sender).getUniqueId().toString());
+                if (args[0].equalsIgnoreCase("del")) {
+                    suggestions.addAll(placeholderMap.keySet());
+                    return suggestions;
+                }
+            }
+        }
+        return suggestions;
     }
 }
